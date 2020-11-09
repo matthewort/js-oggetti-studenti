d54c067e536127bb5c9fbf2e7b2f40df9ed23df0
Matteo Ferrarese <matteo.ferrarese@yahoo.it>
2020-11-09 14:54:41 +0100

    Prima parte

---
+++ js/myscript.js
@@ -0,0 +1,22 @@
+// Oggetti
+//
+//     - Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+//     - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+//     - Dare la possibilità all’utente attraverso 3 prompt di
+//     aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+//
+// Possiamo fare anche sullo stesso file o comunque stessa cartella, se ci pensate può essere una sequenza sullo stesso ex.Anche nel punto 2 gli oggetti studenti che stasnno nell’array hanno almeno le 3 proprietà come in quello iniziale.Bonus
+//
+//     se volete anadate di interazione e/o layout più ricco;
+//     e/o gestione casi limite;
+//     ciò che volete provare.
+
+var studente = {
+  'nome': 'guglielmo',
+  'cognome': 'ansaldo',
+  'età': '32'
+};
+
+for(var key in studente) {
+  console.log(studente[key]);
+}
